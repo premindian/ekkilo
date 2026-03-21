117e4e382cee8f3426eadd5e694102c6aa32992b
Prem Kumar Konatham <premindian2025@gmail.com>
2026-03-21 08:50:29 +0530

    db commit

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,35 @@
 import asyncpg
 import os
 
-DB_URL = os.getenv("DATABASE_URL", "postgresql://postgres:$Aman@localhost:5432/kirana")
+_pool = None
 
-pool = None
+class DB:
+    def __init__(self, pool):
+        self.pool = pool
+
+    async def fetch(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, *args)
+
+    async def fetchrow(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow(query, *args)
+
+    async def execute(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.execute(query, *args)
 
 
 async def get_db():
-    global pool
-    if not pool:
-        pool = await asyncpg.create_pool(DB_URL)
-    return pool
+    global _pool
+
+    if _pool is None:
+        DATABASE_URL = os.getenv("DATABASE_URL")
+
+        _pool = await asyncpg.create_pool(
+            DATABASE_URL,
+            min_size=1,
+            max_size=5
+        )
+
+    return DB(_pool)
